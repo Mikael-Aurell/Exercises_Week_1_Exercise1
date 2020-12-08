73a0dfead9cda819a040e0c5670427934c45cf2d
Mikael Aurell <aurell.mikael@gmail.com>
2020-12-08 11:44:11 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/MikaelAurell/App.java
@@ -0,0 +1,16 @@
+package se.lexicon.MikaelAurell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        //EXERCISE 1
+        System.out.println( "Hello World!" );
+        System.out.println( "Mikael Aurell" );
+    }
+}
